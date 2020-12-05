e0a166116a6d0a15f83ec85101555f5e1840ea9e
gui <ges.cs01@gmail.com>
2020-12-05 16:37:56 -0300

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
 
+const mockUserData= [
+    {name:"Ollie"},
+    {name:"Kickflip"}
+]
+
+app.get('/users', function(req,res){
+    res.json({
+        success: true,
+        message: 'successfully got users!',
+        users: mockUserData
+    })
+});
+
 app.listen(8000, function() {
 console.log("server is running")
 });
