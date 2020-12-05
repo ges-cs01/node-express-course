8b102c1bb9b5b1d9990ebef4ed7f0c1623c10697
gui <ges.cs01@gmail.com>
2020-12-05 17:05:05 -0300

    add POST route

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 const mockUserData= [
     {name:"Ollie"},
@@ -12,7 +15,7 @@ app.get('/users', function(req,res){
         message: 'successfully got users!',
         users: mockUserData
     })
-});
+})
 
 app.get('/users/:id',function(req,res){
     console.log(req.params.id)
@@ -23,7 +26,30 @@ app.get('/users/:id',function(req,res){
     })
 })
 
+app.post('/login', function(req,res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const mockUsername="Jack";
+    const mockPassword="Jane";
+
+    if(username===mockUsername && password===mockPassword) {
+        res.json({
+            success: true,
+            message: 'user and psswd match',
+            token: 'encrypted token'
+        })
+    }
+
+    else {
+        res.json({
+            success: false,
+            message: 'user or psswd do not match'
+        })
+    }
+})
+
 app.listen(8000, function() {
 console.log("server is running")
-});
+})
 
